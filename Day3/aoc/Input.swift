ca39314a3da40645951680d6a17976c8b8848d87
Allan Shortlidge <tshortli@gmail.com>
2020-12-02 20:47:03 -0800

    Day 3: Create project.

---
+++ Day3/aoc/Input.swift
@@ -0,0 +1,12 @@
+//
+//  Input.swift
+//  aoc
+//
+//  Created by Allan Shortlidge on 12/2/20.
+//
+
+import Foundation
+
+let input =
+"""
+"""
---
+++ Day3/aoc/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  aoc
+//
+//  Created by Allan Shortlidge on 11/29/20.
+//
+
+import AdventOfCode
+import Foundation
+
+print("\(input)")
