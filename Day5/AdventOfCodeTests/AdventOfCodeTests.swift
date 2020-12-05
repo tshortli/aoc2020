6bc3da779f420d4a10cb4fefdafde6f60975678f
Allan Shortlidge <tshortli@gmail.com>
2020-12-04 20:58:28 -0800

    Day 5: Create project.

---
+++ Day5/AdventOfCodeTests/AdventOfCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeTests.swift
+//  AdventOfCodeTests
+//
+//  Created by Allan Shortlidge on 11/29/20.
+//
+
+import AdventOfCode
+import XCTest
+
+class AdventOfCodeTests: XCTestCase {
+
+    func testExample() throws {
+        HelloWorld.print()
+    }
+
+}
