8ec6f4053170057601e297557bfb032669a4dad2
Allan Shortlidge <tshortli@gmail.com>
2020-12-04 23:16:12 -0800

    Day 5: Shorter, more elegant code.

--- Day5/AdventOfCode/BoardingPassProcessor.swift
+++ Day5/AdventOfCode/BoardingPassProcessor.swift
@@ -9,34 +9,18 @@ import Foundation
 
 public struct BoardingPassProcessor {
     let lines: [String]
-    let maxIncrement: Int
     
     public init(input: String) {
         self.lines = input.components(separatedBy: .newlines)
-        
-        // Make sure the lines have homogeneous length.
-        let lengths = self.lines.reduce(into: Set<Int>()) { $0.insert($1.count) }
-        guard let length = lengths.first, lengths.count == 1 else {
-            fatalError("Invalid input")
-        }
-        self.maxIncrement = Int(pow(2.0, Double(length - 1)))
     }
     
     public func seat(for string: String) -> Int {
         // The strings form numbers in binary, so we can build the seat ID by adding decreasing powers of 2.
-        var increment = maxIncrement
-        func multiplier(_ character: Character) -> Int {
-            switch character {
-            case "F", "L":   return 0
-            case "B", "R":   return 1
-            default:    fatalError("Unexpected char \(character)")
-            }
-        }
+        var increment = 1 << string.count
         
         return string.reduce(0) { seat, char in
-            let prevIncrement = increment
             increment /= 2
-            return seat + multiplier(char) * prevIncrement
+            return seat + (char == "B" || char == "R" ? increment : 0)
         }
     }
     
