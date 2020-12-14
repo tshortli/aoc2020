2b975c18da2ab86335941cf7708660fcb260473a
Allan Shortlidge <tshortli@gmail.com>
2020-12-13 20:54:12 -0800

    Day 14: Create project.

---
+++ Day14/AdventOfCodeTests/SolverTests.swift
@@ -0,0 +1,17 @@
+//
+//  SolverTests.swift
+//  AdventOfCodeTests
+//
+//  Created by Allan Shortlidge on 12/5/20.
+//
+
+import AdventOfCode
+import XCTest
+
+class SolverTests: XCTestCase {
+
+    func testExample() throws {
+
+    }
+
+}
