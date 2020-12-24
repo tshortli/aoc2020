7216dd1b988b5ad09147140baa2a5e464e4acb5b
Allan Shortlidge <tshortli@gmail.com>
2020-12-24 01:07:56 -0800

    Day 17: Add timing.

--- Day17/aoc/main.swift
+++ Day17/aoc/main.swift
@@ -8,7 +8,10 @@
 import AdventOfCode
 import Foundation
 
+let startDate = Date()
 let game = GameOfLife(input: input)
 
 print("\(game.activeCubesAfterSimulationFixedW())")
 print("\(game.activeCubesAfterSimulation())")
+
+print("Finished in \(Int(Date().timeIntervalSince(startDate) * 1000)) ms")
