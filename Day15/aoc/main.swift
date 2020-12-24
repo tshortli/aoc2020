07d9b58d1bd0d7337a76406c2c33957a497afcef
Allan Shortlidge <tshortli@gmail.com>
2020-12-24 01:21:04 -0800

    Day 15: Add timing.

--- Day15/aoc/main.swift
+++ Day15/aoc/main.swift
@@ -8,7 +8,10 @@
 import AdventOfCode
 import Foundation
 
+let startDate = Date()
 let speaker = NumberSpeaker(input: input)
 
 print("\(speaker.numberSpoken(on: 2020))")
 print("\(speaker.numberSpoken(on: 30_000_000))")
+
+print("Finished in \(Int(Date().timeIntervalSince(startDate) * 1000)) ms")
